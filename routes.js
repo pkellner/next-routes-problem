06930d5bd9b81ac120185165c4a757f9a32ebe86
pkellner <peter@peterkellner.net>
2018-09-22 14:07:45 -0700

    wip

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 const nextRoutes = require('next-routes');
 const routes = module.exports = nextRoutes();
 
-routes.add('presenterallcurrent', '/presenter/:ccYear','speaker');
-routes.add('speakersessiondetail', '/presenter/:ccYear/:slugSpeaker','speakerdetail');
 
-// /presenter/2018
-// /presenter/2018/douglas-crockford-1124
+
+// routes.add('presenterallcurrent', '/presenter/:ccYear','speaker');
+// routes.add('speakersessiondetail', '/presenter/:ccYear/:slugSpeaker','speakerdetail');
+// // /presenter/2018
+// // /presenter/2018/douglas-crockford-1124
 
--- server.js
+++ server.js
@@ -3,8 +3,30 @@ const routes = require('./routes');
 const app = next({dev: process.env.NODE_ENV !== 'production'});
 const handler = routes.getRequestHandler(app);
 
-// With express
-const express = require('express');
-app.prepare().then(() => {
-    express().use(handler).listen(3000)
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+// doing sleep here is similar to what happens if you want to grab some URLs for example from
+// a rest service to add to the router for perf reasons.
+sleep(5000).then(() => {
+
+    routes.add('presenterallcurrent', '/presenter/:ccYear','speaker');
+    routes.add('speakersessiondetail', '/presenter/:ccYear/:slugSpeaker','speakerdetail');
+    // /presenter/2018
+    // /presenter/2018/douglas-crockford-1124
+
+    const express = require('express');
+    app.prepare().then(() => {
+        express().use(handler).listen(3000)
+    });
+
 });
+
+//
+//
+// console.log("about to call express");
+// const express = require('express');
+// app.prepare().then(() => {
+//     express().use(handler).listen(3000)
+// });
