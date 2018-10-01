91fb463c6a85044cad780188c0a431b80d8fcb20
pkellner <peter@peterkellner.net>
2018-10-01 12:43:20 -0700

    Added next.config.js withCss causing localhost:3000 not to start

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// const withCSS = require('@zeit/next-css')
+// /* Without CSS Modules, with PostCSS */
+// module.exports = withCSS();
+
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS({/* my next config */})
